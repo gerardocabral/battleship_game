5744a8e4f3db44fc2aeed3a595d5b6025f58bab9
Nathan Vu <nathan.vu5@gmail.com>
2017-02-08 13:22:24 -0800

    first epic #6, some styling

--- battleship.js
+++ battleship.js
@@ -2,11 +2,21 @@ $(document).ready(function(){
  for (var r = 0; r < 10; r++) {
   $("table").append('<tr id="' + r + '"></tr>')
   for (var c = 0; c < 10; c++) {
-     $('#' + r).append('<td class="tile"></td>')
+     $('#' + r).append('<td id="' + r + c + '" class="tile"></td>')
   }
  }
   $("td").on("click", function() {
+    var row = $(this).attr("id") [0];
+    var col = $(this).attr("id") [1];
     $("#torpedoCount").text("Torpedo Count: " + count++)
+    if (board[row][col] == 0){
+      $("#miss").text("MISS");
+    }else if(board[row][col] == 1) {
+      $("#hit").text("HIT");
+    }
+    // $(this).click(function () {
+    //   $(".hitMiss").hide();
+    // })
     //turn off the click on this TD
     // $("td").click(function(){} --> worked after commenting this out becuase it was making you click twice before triggering the "off click"
       $(this).off("click");
@@ -14,6 +24,7 @@ $(document).ready(function(){
     $(this).addClass("hit");
   })
  var count = 1;
+
 });
 
 //'<tr id="' + row + '">'
@@ -21,3 +32,8 @@ $(document).ready(function(){
 
 // create a counter to keep track of how many torpedos the user has left
 //num = +$(".counter").val();
+
+
+//.on click let the user know if the ship was hit or miss
+// board[row][col] == 0 ---> miss
+// board[row][col] == 1 ---> hit
